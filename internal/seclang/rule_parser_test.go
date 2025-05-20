0e624698b273e73c7dcca5420c589c85181bf973
soujanyanmbri <54130357+soujanyanmbri@users.noreply.github.com>
2025-05-20 13:41:58 +0530

    minor fix

--- internal/seclang/rule_parser_test.go
+++ internal/seclang/rule_parser_test.go
@@ -309,7 +309,7 @@ func TestNonSelectableCollection(t *testing.T) {
 	err := p.FromString(`
 	SecRule REQUEST_URI:foo "bar" "id:1,phase:1"
 	`)
-	if err != nil {
+	if err == nil {
 		t.Error("Unexpected Error", err)
 	}
 }
